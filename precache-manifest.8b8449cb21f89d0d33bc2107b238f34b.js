eac29b27e850a3cef8ce0100923c4c3661252292
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-09-01 17:54:39 +0300

    Updates

---
+++ precache-manifest.8b8449cb21f89d0d33bc2107b238f34b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2473c52c1c6fdd7e51e6d34d7dbb1b6e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "388bd0770761864ff65e",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "9e2327303e333c51ea60",
+    "url": "./static/js/2.acbe9af7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.acbe9af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388bd0770761864ff65e",
+    "url": "./static/js/main.da4d7c85.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
